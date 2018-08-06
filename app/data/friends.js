2548f995fc808e235032ec718c9603e9e7cd459d
Alicia Collins <alicia.collins60@me.com>
2018-08-06 08:34:25 -0700

    updated with sorting logic

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 
 // 5. You should save your application's data inside of `app/data/friends.js` as an array of objects. Each of these objects should roughly follow the format below.
 
-var friendArray=[
+var friendData=[
 {
   "name":"Ahmed",
   "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
@@ -17,7 +17,107 @@ var friendArray=[
       4,
       1
     ]
-}
-];
+},
+{
+  "name":"Cortney",
+  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "scores":[
+      2,
+      5,
+      2,
+      5,
+      5,
+      2,
+      2,
+      1,
+      2,
+      2
+    ]
+},
+{
+  "name":"Charley",
+  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "scores":[
+      3,
+      3,
+      3,
+      2,
+      2,
+      2,
+      2,
+      2,
+      2,
+      2
+  ]
+},
+{
+  "name":"Alicia",
+  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "scores":[
+      3,
+      1,
+      5,
+      4,
+      5,
+      2,
+      2,
+      3,
+      3,
+      1
+    ]
+},
 
-module.exports = friendArray;
+];
+{
+ var test =[
+      2,
+      2,
+      2,
+      2,
+      2,
+      2,
+      2,
+      2,
+      2,
+      2
+    ]
+}
+
+
+  
+var Diff = [];
+var totalDifference=100
+console.log("test",test);
+
+friendData.forEach(function(item,index){
+   
+  for(var i = 0; i < 10; i++){
+  Diff.push(Math.abs(parseInt(test)-parseInt(item.scores[i])));
+  }
+  console.log('Diff in loop',Diff);
+  console.log(
+    Diff.reduce((a, b) => a + b)
+  )
+
+  iDifference=Diff.reduce((a, b) => a + b)
+
+  if(totalDifference>iDifference){
+    totalDifference = iDifference
+    matchName =item.name;
+    matchImg = item.photo;
+
+  }
+
+  Diff = [];
+  return Diff;
+  
+});
+
+console.log("totalDifference",totalDifference);
+console.log("friendArrName", matchName);
+console.log("friendArrPhotoloop",matchImg)
+
+
+
+
+module.exports = friendData;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,22 +1,18 @@
 var friendData = require ("../data/friends");
-var Friends = [];
+
 module.exports = function(app) {
     
-    app.get("/survey", function (req, res) {
-        res.sendFile(path.join(__dirname, "survey.html"));
-    });
+
 //    * A GET route with the url `/api/friends`. This will be used to display a JSON of all possible friends.
     app.get("/api/friends", function (req, res) {
-        return res.json(Friends);
+        return res.json(friendData);
     });
     // A POST routes `/api/friends`. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic. 
     app.post("/api/friends", function (req, res) {
-        var newFriend = req.body;
-        newFriend.routeName = 
-        console.log(newFriend);
-        Friends.push(newFriend);
-        //or Friends.push(req.body);
+
+        friendData.push(req.body);
     })
+    console.log(friendData);
 
 };
 
